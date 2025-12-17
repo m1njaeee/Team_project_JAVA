f63e3d8861dff84bd76a095b7f36caefc210f49e
m1njaeee <alswo0867@naver.com>
2025-12-17 20:09:24 +0900

    ver.최종

--- TeamProjectJava/src/maze/MinotaurMaze.java
+++ TeamProjectJava/src/maze/MinotaurMaze.java
@@ -81,6 +81,7 @@ public class MinotaurMaze {
             }
         }
     }
+    
     //시작 화면
     static void printStartScreen() {
         System.out.println("--------------------------------------");
